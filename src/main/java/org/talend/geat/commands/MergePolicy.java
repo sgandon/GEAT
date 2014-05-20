5a40e10b0f83618762c1ac0d57d562d7e6f3a62d
Stef Mallet <stephane.mallet@gmail.com>
2014-05-20 18:28:25 +0200

    Javadoc: add javadoc in MergePolicy

--- src/main/java/org/talend/geat/commands/MergePolicy.java
+++ src/main/java/org/talend/geat/commands/MergePolicy.java
@@ -1,5 +1,9 @@
 package org.talend.geat.commands;
 
+/**
+ * Defines all available merge policies when 'merging' a branch on another.
+ */
 public enum MergePolicy {
-    REBASE, SQUASH;
+    REBASE, // All commits from source branch will be redo on target branch
+    SQUASH; // Commits from source branch will be squashed to a single commit on target branch
 }
